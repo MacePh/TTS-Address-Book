be81644743359f9a007004a43c32f619b6b6ec80
MacePh <115651516+MacePh@users.noreply.github.com>
2022-12-06 15:22:53 -0500

    Adding functionality

--- Java_Address_Book/src/address_Book/Menu.java
+++ Java_Address_Book/src/address_Book/Menu.java
@@ -7,58 +7,70 @@
 //Create Address Book; Empty
 		
 		public static ArrayList<Contact> contactList  = new ArrayList<>();
+
 		
 		public static void main(String[] args) {
-//			addEntry();
+			goToMenu();
+		}
 			
-		
-		
-//			Scanner input = new Scanner(System.in);  // Create a Scanner object
-}
-//			System.out.println("What action would you like to perform?");
-//			System.out.println("");
-//			System.out.println("1) Add an entry");
-//			System.out.println("");
-//			System.out.println("2) Remove an entry");
-//			System.out.println("");
-//			System.out.println("3) Search for a specific entry");
-//			System.out.println("");
-//			System.out.println("4) Print Address Book");
-//			System.out.println("");
-//			System.out.println("5) Delete Book");
-//			System.out.println("");
-//			System.out.println("6) Quit");
-//			System.out.println("");
-//			System.out.println("Please enter a number 1-6 to make your selection.");
+			
+			public static void goToMenu() {
+			try(Scanner input = new Scanner(System.in)){  // Create a Scanner object
+
+			System.out.println("What action would you like to perform?");
+			System.out.println("");
+			System.out.println("1) Add an entry");
+			System.out.println("");
+			System.out.println("2) Remove an entry");
+			System.out.println("");
+			System.out.println("3) Search for a specific entry");
+			System.out.println("");
+			System.out.println("4) Print Address Book");
+			System.out.println("");
+			System.out.println("5) Delete Book");
+			System.out.println("");
+			System.out.println("6) Quit");
+			System.out.println("");
+			System.out.println("Please enter a number 1-6 to make your selection.");		
+			int userChoice = input.nextInt();	
+		//passing variable to the switch
 //			
-//			int userChoice = input.nextInt();
-//			
-//			//passing variable to the switch
-//			
-//			switch (userChoice)
-//			{
+			switch (userChoice)
+			{
 //			//comparing value of variable against each case
-//			case 1:
-//				//execute "1) Add an entry");
-//				break;
-//			case 2:
+			case 1:
+				//execute "1)
+				addEntry();
+				break;
+			case 2:
 //				// execute "2) Remove an entry");
-//				break;
-//			case 3: 
-//				//"3) Search for a specific entry");
-//				break;
-//			case 4:
-//				//"4) Print Address Book");
-//				break;
-//			case 5:
-//				//"5) Delete Book");
-//				break;
-//			case 6: 
-//				//quit
-//				break;
-//			default:
-//		          System.out.println("Invalid Input!");
-//			}
+				removeEntry();
+				break;
+			case 3: 
+				//"3) Search for a specific entry");
+				searchEntry();
+				break;
+			case 4:
+				//"4) Print Address Book");
+				printAddressBook();
+				break;
+			case 5:
+				//"5) Delete Book");
+				deleteBook();
+				break;
+			case 6: 
+				//quit
+				quit();
+				break;
+			default:
+		          System.out.println("Invalid Input!");
+		          }
+			}
+			
+		}
+	
+		
+			
 //			/*Starting the Program 
 //			Upon starting the program, it should as the user what action they want to perform. 
 //			1) Add an entry 
@@ -72,37 +84,11 @@
 //
 //		}
 //		//Asks user for Contact details
-////		
-		
-//		public static void addEntry() {
-////			printBar(numBars:1);
-//			try (Scanner input = new Scanner(System.in)) {
-//			Contact contact = new Contact(null, null, null, null);
-//			System.out.println("Adding Contact to Address Book");
-//			
-//			System.out.println("Enter first name");
-//			contact.setFName(input.next());
-//			
-//			System.out.println("Enter last name");
-//			contact.setLName(input.next());
-//			
-//			System.out.println("Enter phone number");
-//			contact.setPhoneN(input.next());
-//			
-//			System.out.println("Enter email address.");
-//			contact.setEmail(input.next());
-//			
-//			//add to contactList
-//			contactList.add(contact);
-//		}
-//			System.out.print(contactList);
-//		}
-		
-//		
+
 			
 		public static void addEntry() {
 			try (Scanner input = new Scanner(System.in)) {
-				
+				printBars(2);
 				System.out.println("Enter first name");
 				String firstName = input.next();
 //				Contact.setFirstName(input.next());
@@ -116,6 +102,7 @@
 				 Contact k = new Contact(firstName, lastName,phoneNumber, email);
 				contactList.add(k);
 			}
+			goToMenu();
 //			System.out.print(contactList);
 		}
 
@@ -127,16 +114,19 @@
 				}
 				printAddressBook();
 				System.out.println("Your welcome!");
+				goToMenu();
 		}
 
-//		public void searchEntry() {
-//			
-//		}
+		public static void searchEntry() {
+			System.out.println("Honestly, search is over-rated...");
+			goToMenu();
+		}
 		public static void printAddressBook() {
 			ListIterator<Contact> litr = contactList.listIterator();
 			while (litr.hasNext()) {
 				System.out.print(litr.next());
 		}
+			goToMenu();
 		}
 		public static void deleteBook() {
 			try (Scanner input = new Scanner(System.in)) {
@@ -151,18 +141,22 @@
 					System.out.println("Thank you!");
 				}
 		}
+			goToMenu();
 		}
 		
-		public void quit() {
+		public static void quit() {
 			System.exit(0);
 		}
-				private static void printBars(int numBars){
-			        for (int i = 0; i < numBars ; i++) {
-			            printBar();
-			        }
-			    }
-				 private static void printBar() {
-				        System.out.println("===========================================");
-				    }
-			}
-//	}
+		
+			    
+	    private static void printBars(int numBars){
+	        for (int i = 0; i < numBars ; i++) {
+	            printBar();
+	        }
+	    }
+
+	    private static void printBar() {
+	        System.out.println("===========================================");
+	    }
+}
+	
