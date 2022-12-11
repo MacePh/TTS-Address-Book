b0a860d159c40c05f53793eeb92cefa3010c5e9b
Phi-Hoang-Ngo <vpgmarrero@outlook.com>
2022-12-10 19:05:43 -0600

    final version with all bells and whistles

--- Java_Address_Book/src/address_Book/AddressBook.java
+++ Java_Address_Book/src/address_Book/AddressBook.java
@@ -14,110 +14,151 @@ public class AddressBook {
 	public static ArrayList<Contact> readList() {
 		try {
 			FileInputStream readData = new FileInputStream("contactList.ser");
-
 			ObjectInputStream readStream = new ObjectInputStream(readData);
-
 			ArrayList<Contact> contactList = (ArrayList<Contact>) readStream.readObject();
 			readStream.close();
-
-		return contactList;
-		} catch (IOException | ClassNotFoundException e) {
+			return contactList;
+		} catch (IOException | ClassNotFoundException | NullPointerException e) {
 			e.printStackTrace();
 		}
 		return contactList;
-	
 	}
 
-	//Create Address Book; Empty
-//	private static final ArrayList<Contact> contactList2 = readList() ;
-	private static  ArrayList<Contact> contactList  = readList();
+	private static ArrayList<Contact> contactList = readList();
 	Iterator<Contact> i = contactList.iterator();
-	Scanner s = new Scanner(System.in);
+	static Scanner s = new Scanner(System.in);
 
-	public void addEntry() {
-//		printBars(2);
-		System.out.println("Enter first name:");
-		String firstName = s.next();
-//		Contact.setFirstName(input.next());
-		System.out.print("Enter last name :");
-		String lastName = s.next();
-		System.out.print("Enter phone number :");
-		String phoneNumber = s.next();
-		System.out.print("Enter email address :");
-		String email = s.next();
-		contactList.add(new Contact(firstName, lastName,phoneNumber, email));
-		saveContact(contactList);
-		
-		System.out.println("=+=+=+=+=+=+=+=+ADDED=+=+=+=+=+=+=+=+=+=");
-	}
+	static public void addEntry() {
+		boolean keepGoing = true;
+		while (keepGoing == true) {
+			String firstName = "";
+			String lastName = "";
+			String phoneNumber = "";
+			String email = "";
+			boolean waitingForFirstName = true;
+			while (waitingForFirstName) {
+				System.out.println("Enter first name");
+				String fName = s.nextLine();
+				if (fName.length() > 2) {
+					firstName = fName;
+					waitingForFirstName = false;
+				} else {
+					System.out.println("Please enter a response that is three letters or longer.");
+				}
+			}
+			boolean waitingForLastName = true;
+			while (waitingForLastName) {
+				System.out.println("Enter last name");
+				String lName = s.nextLine();
+				if (lName.length() > 2) {
+					lastName = lName;
+					waitingForLastName = false;
+				} else {
+					System.out.println("Please enter a response that is three letters or longer.");
+				}
+			}
 
-	public  void removeEntry() {
-	boolean found=false;
-	printAddressBook();
-	String fDel= s.next();
-	Iterator<Contact> i = contactList.iterator();
-	while(i.hasNext()) {
-		Contact c = (Contact) i.next();
-		if(c.getFirstName().equals(fDel)) {
-			i.remove();
+			boolean waitingForPhoneNumber = true;
+			while (waitingForPhoneNumber) {
+				System.out.println("Enter phone number");
+				String digits = s.nextLine();
+				if (digits.length() > 6) {
+					phoneNumber = digits;
+					waitingForPhoneNumber = false;
+				} else {
+					System.out.println("Please enter a response that is 6 numbers or longer.");
+				}
+			}
+
+			boolean waitingForEmail = true;
+			while (waitingForEmail) {
+				System.out.println("Enter email address");
+				String correo = s.nextLine();
+				if (isValid(correo) == true) {
+					email = correo;
+					waitingForEmail = false;
+					keepGoing = false;
+				} else {
+					System.out.println("Please enter a valid email address.");
+				}
+
+			}
+
+			contactList.add(new Contact(firstName, lastName, phoneNumber, email));
 			saveContact(contactList);
-			found = true;
+
+			System.out.println("=+=+=+=+=+=+=+=+ADDED=+=+=+=+=+=+=+=+=+=");
+
 		}
 	}
-	if(!found) {
-		System.out.println("=+=+=+=+=+=+=+=+NOT=FOUND=+=+=+=+=+=+=+=+=+=\n");
 
-	}
-	else {
-		
-	}
-	System.out.println("=+=+=+=+=+=+=+=+=+DELETED=+=+=+=+=+=+=+=\n");
-	}
-		
-	public  void searchEntry() {
-	boolean found=false;
-	System.out.println("Enter First Name to search");
-	String fSearch= s.nextLine();
-	i = contactList.iterator();
-	while(i.hasNext()) {
-		Contact c = i.next();
-		if(c.getFirstName().equals(fSearch)) {
-			System.out.println("Found: "+c);
-			found = true;
+	public void removeEntry() {
+		boolean found = false;
+		printAddressBook();
+		String fDel = s.next();
+		Iterator<Contact> i = contactList.iterator();
+		while (i.hasNext()) {
+			Contact c = (Contact) i.next();
+			if (c.getFirstName().equals(fDel)) {
+				i.remove();
+				saveContact(contactList);
+				found = true;
+			}
 		}
-	}if(!found) {
-		System.out.println("=+=+=+=+=+=+=CONTACT=NOT=FOUND=+=+=+=+=+=+=\n");
-	}
-	System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
-	}
-	
-	public  void printAddressBook() {
-	System.out.println("Address book contains :");
-	ListIterator<Contact> litr = contactList.listIterator();
-	while (litr.hasNext()) {
-		System.out.print(litr.next());
-}
-	System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
-}
- 
-	public  void deleteBook() {
+		if (!found) {
+			System.out.println("=+=+=+=+=+=+=+=+NOT=FOUND=+=+=+=+=+=+=+=+=+=\n");
 
-	System.out.println("Delete ENTIRE address book? ?\n 'y': Delete it. \n 'n': No, keep it.");
-	String userInput = s.nextLine();
-	
-	if (userInput.equalsIgnoreCase("y")) {
-		System.out.println("=+=+=+=+=+=+ADDRESS+BOOK=DELETED=+=+=+=+=+=\n");
+		} else {
+
+		}
+		System.out.println("=+=+=+=+=+=+=+=+=+DELETED=+=+=+=+=+=+=+=\n");
+
+	}
+
+	public void searchEntry() {
+		boolean found = false;
+		System.out.println("Enter First Name to search");
+		String fSearch = s.nextLine();
+		i = contactList.iterator();
+		while (i.hasNext()) {
+			Contact c = i.next();
+			if (c.getFirstName().equals(fSearch)) {
+				System.out.println("Found: " + c);
+				found = true;
+			}
+		}
+		if (!found) {
+			System.out.println("=+=+=+=+=+=+=CONTACT=NOT=FOUND=+=+=+=+=+=+=\n");
+		}
+		System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+	}
+
+	public void printAddressBook() {
+		System.out.println("Address book contains :");
+		contactList = readList();
+		ListIterator<Contact> litr = contactList.listIterator();
+		while (litr.hasNext()) {
+			System.out.print(litr.next());
+		}
+		System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
+	}
+
+	public void deleteBook() {
+
+		System.out.println("Delete ENTIRE address book? ?\n 'y': Delete it. \n 'n': No, keep it.");
+		String userInput = s.nextLine();
+
+		if (userInput.equalsIgnoreCase("y")) {
+			System.out.println("=+=+=+=+=+=+ADDRESS+BOOK=DELETED=+=+=+=+=+=\n");
 			contactList.clear();
-		} 
-	else if(userInput.equalsIgnoreCase("n")) {
-		System.out.println("=+=+=+=+=+=+NOT+DELETED+=+=+=+=+=+=+=+=+=+=\n");
+			saveContact(contactList);
+		} else if (userInput.equalsIgnoreCase("n")) {
+			System.out.println("=+=+=+=+=+=+NOT+DELETED+=+=+=+=+=+=+=+=+=+=\n");
+		}
+		System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
 	}
-	System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n");
 
-}
-
-
-	public void saveContact(ArrayList<Contact> contactList) {
+	public static void saveContact(ArrayList<Contact> contactList) {
 		try {
 			FileOutputStream writeData = new FileOutputStream("contactList.ser");
 			ObjectOutputStream writeStream = new ObjectOutputStream(writeData);
@@ -131,5 +172,9 @@ public class AddressBook {
 		}
 	}
 
-}
+	static boolean isValid(String email) {
+		String regex = ".+\\@.+\\..+";
+		return email.matches(regex);
+	}
 
+}
